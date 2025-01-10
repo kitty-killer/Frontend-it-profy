b0f33ef7e402e67e0b4a7e5868b4965abe82672f
kitty-killer <124335721+kitty-killer@users.noreply.github.com>
2025-01-10 19:13:28 +0300

    4.js

--- js_3-4/4.js
+++ js_3-4/4.js
@@ -28,7 +28,7 @@ for (const name in engineers) {
 
 const array = [1, 2, 3, 4, 5];  
 
-for (let i = 1; i < array.length; i += 2) {
+for (let i = 0; i < array.length; i += 2) {
     console.log(array[i]);
 }
 
