1a4422764c196834fdbe28a2cbcb4d09ea81046a
kitty-killer <124335721+kitty-killer@users.noreply.github.com>
2025-02-15 11:38:19 +0300

    Update clock.js

--- time/clock.js
+++ time/clock.js
@@ -30,68 +30,15 @@ const interval = setInterval(() => {
 
 const deg = 6;
 const hr = document.querySelector('#hr');
-const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
-const finishElement = document.querySelector('#finish');
-const endElement = document.querySelector('.end');
-const nextDayElement = document.querySelector('#nextDay');
-
 
 setInterval(() => {
-    const day = new Date();
-    const hours = day.getHours();
-    const minutes = day.getMinutes();
-    const seconds = day.getSeconds();
+    let day = new Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
 
-    const hh = hours * 30;
-    const mm = minutes * deg;
-    const ss = seconds * deg;
-
-    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
-    mn.style.transform = `rotateZ(${mm}deg)`;
-    sc.style.transform = `rotateZ(${ss}deg)`;
-
-function time(t) {
-    const hrs = Math.floor((t / (1000 * 60 * 60)) % 24);
-    const mins = Math.floor((t / (1000 * 60)) % 60);
-    const secs = Math.floor((t / 1000) % 60);
-
-    return {
-        hrs: hrs.toString().padStart(2, '0'),
-        mins: mins.toString().padStart(2, '0'),
-        secs: secs.toString().padStart(2, '0')
-    };
-}
-
-    const endOfWork = new Date(day.getFullYear(), day.getMonth(), day.getDate(), 21, 0, 0);
-    const remainingTime = endOfWork - day;
-
-    if (day.getDay() === 2 || day.getDay() === 5) {
-        if (remainingTime > 0 && hours >= 19) {
-            const { hrs, mins, secs } = time(remainingTime);
-
-            finishElement.textContent = `${hrs}:${mins}:${secs}`;
-            nextDayElement.textContent = '';
-        } else {
-            finishElement.style.display = 'none';
-            endElement.textContent = "Рабочий день окончен";
-
-            let nextWorkDay = new Date(day);
-            nextWorkDay.setHours(19, 0, 0, 0);
-            if (day.getDay() === 2) {
-                nextWorkDay.setDate(day.getDate() + 3);
-            } else {
-                nextWorkDay.setDate(day.getDate() + 4);
-            }
-
-            const remainingToNextDay = nextWorkDay - day;
-            const { hrs, mins, secs } = time(remainingToNextDay);
-
-            nextDayElement.textContent = `До следующего рабочего дня осталось: ${hrs}:${mins}:${secs}`;
-        }
-    } else {
-        finishElement.style.display = 'none';
-        endElement.textContent = "Сегодня выходной!";
-        nextDayElement.textContent = '';
-    }
-}, 1000);
+hr.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+})
